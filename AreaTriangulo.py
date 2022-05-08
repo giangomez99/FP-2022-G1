ac3a5f40302873e497d04ef440d4773e70f28828
GianFrancoFran5 <>
2022-05-08 19:20:57 +0000

    trabajo en distinto lenguajes de programacion de gitignore

---
+++ AreaTriangulo.py
@@ -0,0 +1,9 @@
+#Definicon de Variables
+print("El area del Triangulo")
+#Datos de Entrada
+b=int(input("Ingrese la base: "))
+h=int(input("Ingrese la altura: "))
+#Proceso
+area=(b*h)/2
+#Datos de Salida
+print("El area del triangulo es: ",area)
